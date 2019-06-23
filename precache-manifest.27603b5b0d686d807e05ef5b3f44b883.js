6cef57fb7e37271ee58e2da6f9b25e69ad78b3fb
Alikhan Gatsalov <alihan_g5@mail.ru>
2019-06-23 05:14:28 +0300

    Updates

---
+++ precache-manifest.27603b5b0d686d807e05ef5b3f44b883.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4b6d1c40f2f30a28ad74f10452973a",
+    "url": "/wma_test/index.html"
+  },
+  {
+    "revision": "182c9cf0f5904849d718",
+    "url": "/wma_test/static/css/main.694f219b.chunk.css"
+  },
+  {
+    "revision": "06092787e4731d816b72",
+    "url": "/wma_test/static/js/2.892ed99d.chunk.js"
+  },
+  {
+    "revision": "182c9cf0f5904849d718",
+    "url": "/wma_test/static/js/main.804c7f3c.chunk.js"
+  },
+  {
+    "revision": "157846ab390ad3ea2516",
+    "url": "/wma_test/static/js/runtime~main.df6e155b.js"
+  },
+  {
+    "revision": "02fc649d65e0fa086a2c55fac3101811",
+    "url": "/wma_test/static/media/Welcome.02fc649d.jpg"
+  },
+  {
+    "revision": "815c0ccf31a4799ba8e95c667dea4ac1",
+    "url": "/wma_test/static/media/bg2.815c0ccf.jpg"
+  },
+  {
+    "revision": "edb5be20406ddb1b8989487087eca603",
+    "url": "/wma_test/static/media/preloader.edb5be20.svg"
+  }
+]);
